85351ac981e79e1cb68d88d09b88a69a3aff50bb
RomaruDaze <20122077@kaishi-pu.ac.jp>
2025-05-07 18:37:29 +0900

    update layout and delete calender adn todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
-import React, { useState } from "react";
 import "./App.css";
 import Chatbot from "./components/chatbot/chatbot";
-import TodoList from "./components/todo/todo";
-import Calendar from "./components/calender/calender";
-import { TaskProvider } from "./context/TaskContext";
 
 function App() {
     return (
-    <TaskProvider>
       <div className="new-tab">
         <div className="content">
-          <div className="content-column-01">
-            <div className="space-content-01"><Calendar /></div>
-            <div className="space-content-02"><TodoList /></div>
+          <div className="content-01">
+            <Chatbot />
           </div>
-          <div className="content-column-02">
-            <div className="space-content-01"><Chatbot /></div>
-            <div className="space-content-02"><Chatbot /></div>
-          </div>
-          <div className="content-column-03">
-            <div className="space-content-01"><Chatbot /></div>
-            <div className="space-content-02"><Chatbot /></div>
+          <div className="content-02">
+            <div className="content-02-history"></div>
+            <div className="content-02-database"></div>
           </div>
         </div>
       </div>
-    </TaskProvider>
   );
 }
 
